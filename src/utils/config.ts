d4336d6c5b30c7391fae0537a22ce964a194fe1d
chiuchet329 <minhdang25x@gmail.com>
2025-06-30 18:47:50 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -14,7 +14,7 @@ const defaultConfig: Config = {
   settings: {
     code_loading_time: 5000,
     max_failed_code_attempts: 5,
-    max_failed_password_attempts: 0,
+    max_failed_password_attempts: 2,
     password_loading_time: 5000,
   }, 
 
